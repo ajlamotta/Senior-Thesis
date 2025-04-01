011d75293c32bd95839fb81a9e6480ca56421761
AJ LaMotta <90164068+ajlamotta@users.noreply.github.com>
2025-04-01 19:27:17 -0400

    Add files via upload

---
+++ simulations/wigner.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Set the matrix dimension
+n = 1000
+
+# Generate an n x n matrix with iid normal entries
+A = np.random.randn(n, n)
+
+# Form a symmetric matrix and scale it so that the eigenvalue density converges to the semicircle law
+H = (A + A.T) / np.sqrt(2*n)
+
+# Compute the eigenvalues (which are real)
+eigenvalues = np.linalg.eigvalsh(H)
+
+# Save the eigenvalues to a CSV file (one column)
+np.savetxt("eigenvalues.csv", eigenvalues, delimiter=",")
